2fdddd084bda2a7379981261e51450ee37cebb1a
Abdul Waheed <abdul.waheed@cognizant.com>
2019-10-15 22:47:55 +0530

    Sql version 2

--- library/src/routes/bookRoutes.js
+++ library/src/routes/bookRoutes.js
@@ -5,54 +5,6 @@ const debug = require('debug')('app');
 
 function router(nav) {
 
-    const books = [{
-        id: 1,
-        title: "Ansible 3.0",
-        genre: "Automation",
-        author: "Mohamed Safwan"
-    },
-    {
-        id: 2,
-        title: "Concourse",
-        genre: "Dev Ops",
-        author: "Aisha Sana"
-    },
-    {
-        id: 3,
-        title: "Mastering Node Applications",
-        genre: "Server Scripting",
-        author: "Mohamed Safwan"
-    },
-    {
-        id: 4,
-        title: "Learning Me",
-        genre: "Biography",
-        author: "Abdul Waheed"
-    },
-    {
-        id: 5,
-        title: "Worst Human Behaviours",
-        genre: "Fiction",
-        author: "Abdul Waheed"
-    },
-    {
-        id: 6,
-        title: "Space Shuttle",
-        genre: "Astro Science",
-        author: "Mohamed Safwan"
-    },
-    {
-        id: 7,
-        title: "Drawing Basics",
-        genre: "Kids Learning",
-        author: "Aisha Sana"
-    },
-    {
-        id: 8,
-        title: "Miserable",
-        genre: "Fiction",
-        author: "Abdul Waheed"
-    }]
     const request = new sql.Request();
 
     //Promise example
@@ -73,12 +25,12 @@ function router(nav) {
     bookRouter.route('/')
         .get((req, res) => {
             (async function () {
-                const result = await request.query('select * from books');
+                const {recordset} = await request.query('select * from books');
                 res.render('bookList',
                     {
                         title: 'Books',
                         nav,
-                        books: result.recordset
+                        books: recordset
                     })
             }());
         });
@@ -86,12 +38,18 @@ function router(nav) {
     bookRouter.route('/:id')
         .get((req, res) => {
             const { id } = req.params;
-            res.render('bookInfo',
-                {
-                    title: books[id].genre,
-                    nav,
-                    book: books[id]
-                })
+            (async function () {
+        
+                const {recordset} = await request.input('id',sql.Int,Number(id)+1)
+                .query('select * from books where id = @id'); 
+             
+                res.render('bookInfo',
+                    {
+                        title: recordset[0].genre,
+                        nav,
+                        book: recordset[0]
+                    })
+            }());
         });
     return bookRouter;
 }
